8b69cb42bf1313f567d889e8dd5d03d4fe562949
RomanSnezhko <roman.snezhko@bk.ru>
2025-02-04 20:21:08 +0300

    add file App.css

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ function App() {
   return (
     <div className="App">
     <h1>  It's my App </h1>
+      Todolist
     </div>
   );
 }
 
 export default App;
 
-
 function Todolist() {
   return <div> TO_DO_lIST </div>
 }
+
+export default Todolist;
