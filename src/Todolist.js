6a927faed9ca12cb2e03acdfc58cb38821d68d71
RomanSnezhko <roman.snezhko@bk.ru>
2025-02-12 20:09:06 +0300

    fix Todolist

--- src/Todolist.js
+++ src/Todolist.js
@@ -13,23 +13,23 @@ type PropsType = {
     cards: Array<CardsType>
 }
 
-export function Todolist(props: PropsType) {
-
-    console.log(props)
-    return (
-        <div>
-            <h3>{props.title}</h3>
-            <div>
-                <input/>
-                <p>
-                    <button>Search</button>
-                </p>
-            </div>
-            <ul>
-                <li><input type="checkbox" checked={true}/><span>{props.cards[0].title}</span></li>
-                <li><input type="checkbox" checked={true}/><span>{props.cards[1].title}</span></li>
-                <li><input type="checkbox" checked={false}/><span>{props.cards[2].title}</span></li>
-            </ul>
-        </div>
-    )
-}
+// export function Todolist(props: PropsType) {
+//
+//     console.log(props)
+//     return (
+//         <div>
+//             <h3>{props.title}</h3>
+//             <div>
+//                 <input/>
+//                 <p>
+//                     <button>Search</button>
+//                 </p>
+//             </div>
+//             <ul>
+//                 <li><input type="checkbox" checked={true}/><span>{props.cards[0].title}</span></li>
+//                 <li><input type="checkbox" checked={true}/><span>{props.cards[1].title}</span></li>
+//                 <li><input type="checkbox" checked={false}/><span>{props.cards[2].title}</span></li>
+//             </ul>
+//         </div>
+//     )
+// }
