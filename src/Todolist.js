53fdb0dbe320e894a0321dee03878e34950df73c
RomanSnezhko <roman.snezhko@bk.ru>
2025-02-14 19:24:28 +0300

    Todolist

--- src/Todolist.js
+++ src/Todolist.js
@@ -14,22 +14,22 @@ type PropsType = {
 }
 
 // export function Todolist(props: PropsType) {
-//
-//     console.log(props)
-//     return (
-//         <div>
-//             <h3>{props.title}</h3>
-//             <div>
-//                 <input/>
-//                 <p>
-//                     <button>Search</button>
-//                 </p>
-//             </div>
-//             <ul>
-//                 <li><input type="checkbox" checked={true}/><span>{props.cards[0].title}</span></li>
-//                 <li><input type="checkbox" checked={true}/><span>{props.cards[1].title}</span></li>
-//                 <li><input type="checkbox" checked={false}/><span>{props.cards[2].title}</span></li>
-//             </ul>
-//         </div>
-//     )
-// }
+
+    console.log(props)
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <p>
+                    <button>Search</button>
+                </p>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/><span>{props.cards[0].title}</span></li>
+                <li><input type="checkbox" checked={true}/><span>{props.cards[1].title}</span></li>
+                <li><input type="checkbox" checked={false}/><span>{props.cards[2].title}</span></li>
+            </ul>
+        </div>
+    )
+}
